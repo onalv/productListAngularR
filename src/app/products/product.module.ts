c79773caff1103baad3c412191a5b5f03f26fe34
onalv <omarnavaalvarez@gmail.com>
2017-11-20 00:30:12 -0600

    changes to routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
@@ -8,13 +9,14 @@ import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductService } from './product.service';
 
 import { SharedModule } from '../shared/shared.module';
-import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
     SharedModule,
       RouterModule.forChild([
-        {path: 'products', component: ProductListComponent}
+        {path: 'products', component: ProductListComponent},
+        {path: 'products/:id', component: ProductDetailComponent},
+        {path: 'products/:id/edit', component: ProductEditComponent}
       ])
   ],
   declarations: [
